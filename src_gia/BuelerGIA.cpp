8edbc935fd0a3fa1f579ebc21c99746ac7cca444
Samuel B Kachuck <skachuck@gmail.com>
2019-02-20 11:01:03 -0500

    Header and code sep, getting near testing

--- src_gia/BuelerGIA.cpp
+++ src_gia/BuelerGIA.cpp
@@ -10,10 +10,8 @@
 #include <fstream>
 #include <fftw3.h>
 
-#include "SurfaceFlux.H"
 #include "BisiclesF_F.H"
 #include "BuelerGIA.H"
-#include "HotspotFlux.H"
 #include "AmrIce.H"
 
 
@@ -54,64 +52,7 @@
  */
 
 
-class BuelerGIAFlux : public SurfaceFlux
-{
-public:
-  BuelerGIAFlux( );
-  virtual ~BuelerGIAFlux();
- 
 
-  /// factory method
-  /** return a pointerto a new SurfaceFlux object
-   */
-  virtual SurfaceFlux* new_surfaceFlux();
-
-  virtual void surfaceThicknessFlux(LevelData<FArrayBox>& a_flux,
-  			    const AmrIceBase& a_amrIce, 
-  			    int a_level, Real a_dt);
-  // Set 1-layer viscosity
-  void setViscosity( Real& a_visc );
-  // Set 2-layer viscosity
-  //void setViscosity( RealVect a_viscvec, Real& a_thk );
-  // Set N-layer viscosity (N>2)
-  //void setViscosity( RealVect a_viscvec, RealVect a_thkvec ); 
-  void setFlexural( Real& a_flex );
-  void setTimestep( Real a_dt );
-  void setDomain( int a_Nx, int a_Ny, Real a_Lx, Real a_Ly );
-  void precomputeGIAstep();
-protected:
-  const Real PI2=2.*3.14159267;
-  const Real SECSPERYEAR=31536000.;
-
-  Real m_flex, m_visc, m_thk;
-  RealVect m_viscvec, m_thkvec;
-  Real m_dt;
-  // physical constants
-  Real m_iceDensity, m_gravity, m_mantleDensity;
-  // Domain constants
-  int m_Nx, m_Ny;
-  Real m_Lx, m_Ly;
-  bool m_isDomainSet;
-  Real m_updatedTime;
-  // Constants for GIA step (computed once, stored) (size Nx x Ny)
-  LevelData<FArrayBox> m_beta, m_gamma;
-  // Other quantities required during computation (size Nx x Ny)
-  LevelData<FArrayBox> m_tafhat0;         // Initial thickness above flotation 
-                                          // (assumes initially isostatic equilbrium).
-  LevelData<FArrayBox> m_taf, m_tafhat;   // Thickness above flotation and FFT'd.
-  LevelData<FArrayBox> m_udot, m_udothat; // Surface velocity and FFT'd.
-  LevelData<FArrayBox> m_uhat;            // FFT'd surface displacements.
-  // FFTW transformations.
-  fftw_plan fftfor_load, fftinv_udot, fftinv_u;
-
-protected:
-  // Check if a velocity field update is necessary.
-  bool updateCheck( Real t );
-  void updateUdot( RealVect Hab );
-  void fftpadfor();
-  void fftinv();
-  
-}
 
 /// factory method
 /** return a pointerto a new SurfaceFlux object
@@ -139,7 +80,7 @@ BuelerGIAFlux::setDomain( int a_Nx, int a_Ny, Real a_Lx, Real a_Ly ) {
   m_Lx = a_Lx;
   m_Ly = a_Ly;
   m_isDomainSet = true;
-
+}
 
 void 
 BuelerGIAFlux::setViscosity( Real& a_visc ) {
@@ -207,12 +148,12 @@ BuelerGIAFlux::precomputeGIAstep() {
   // We use real-to-real (discrete hartley) transformations.
   // Note: FFTW in column-major order by swapping order of Nx, Ny. 
   // Note: Inverse fft needs to be normalized by N.
-  fftfor_load = fftw_plan_r2r_2d(Ny, Nx, &dL[0][0], &dLhat[0][0], 
+  fftfor_load = fftw_plan_r2r_2d(Ny, Nx, &m_taf[0][0], &m_tafhat[0][0], 
                                   FFTW_DHT, FFTW_DHT, FFTW_ESTIMATE);
-  fftinv_udot = fftw_plan_r2r_2d(Ny, Nx, &Uhatdot[0][0], &Udot[0][0], 
+  fftinv_udot = fftw_plan_r2r_2d(Ny, Nx, &m_uhatdot[0][0], &m_uhat[0][0], 
                                   FFTW_DHT, FFTW_DHT, FFTW_ESTIMATE);
-  fftinv_u = fftw_plan_r2r_2d(Ny, Nx, &Uhatn[0][0], &Un[0][0], 
-                                            FFTW_DHT, FFTW_DHT, FFTW_ESTIMATE);
+  //fftinv_u = fftw_plan_r2r_2d(Ny, Nx, &m_uhat[0][0], &m_u[0][0], 
+  //                                          FFTW_DHT, FFTW_DHT, FFTW_ESTIMATE);
 
   IntVect loVect = IntVect::Zero;
   IntVect hiVect(Nx-1, Ny-1);
@@ -234,21 +175,35 @@ BuelerGIAFlux::precomputeGIAstep() {
 
   Real kx, ky, kij, tau, alpha_l;
 
-  for (int i=0;i<Nx;i++){
-    for (int j=0;j<Ny;j++){
-      kx = PI2*min(i,m_Nx-i)/m_Nx*(m_Nx-1)/m_Lx;
-      ky = PI2*min(j,m_Ny-j)/m_Ny*(m_Nx-1)/m_Ly;
-      kij = sqrt(pow(kx,2),pow(ky,2));
+  DataIterator dit = dbl.dataIterator();
+  for (dit.begin();dit.ok();++dit) {
+    // grab this FAB
+    FArrayBox& m_betaFAB = m_beta[dit];
+    FArrayBox& m_gammaFAB = m_gamma[dit];
+
+    BoxIterator bit(m_betaFAB.box());
+    for (bit.begin(); bit.ok(); ++bit) {
+      IntVect iv = bit();
+      int comp = 0;
+
+//  for (int i=0;i<Nx;i++){
+//    for (int j=0;j<Ny;j++){
+      kx = PI2*min(iv[0],m_Nx-i)/m_Nx*(m_Nx-1)/m_Lx;
+      ky = PI2*min(iv[1],m_Ny-j)/m_Ny*(m_Nx-1)/m_Ly;
+      kij = sqrt(pow(kx,2)+pow(ky,2));
       // The lithosphere filter.
       alpha_l = 1. + pow(kij,4)*m_flex/m_mantleDensity/m_gravity;
       // The explonential viscous relaxation time constant.
       tau = 2*m_visc*kij/m_gravity/m_mantleDensity/alpha_l;
       // The Bueler et al., 2007 fields.  
-      m_beta[i][j] = m_mantleDensity*m_gravity + m_flex*pow(kij,4);
-      m_gamma[i][j] = pow((m_beta[i][j]*(tau[i][j] + 0.5*m_dt*SECSPERYEAR)),-1);
+      m_betaFAB(iv,comp) = m_mantleDensity*m_gravity + m_flex*pow(kij,4);
+      m_gammaFAB(iv,comp) = pow((m_betaFAB(iv,comp)*(tau + 0.5*m_dt*SECSPERYEAR)),-1);
+
+      if (iv[0] == 0 && iv[1] == 0) {
+        m_gammaFAB(iv,comp) = 0.;
+      }
     }
   }
-  m_gamma[0][0] = 0.;
 }
 
 bool BuelerTopgFlux::updateCheck(Real time) {
@@ -276,12 +231,32 @@ BuelerTopgFlux::updateUdot( const AmrIceBase& a_amrIce ) {
 
   // FFT forward transform the load
   fftpadfor();
-  // Update transformed velocity and uplift fields using Bueler, et al. 2007, eq 11.
-  for (int i=0;i<Nx;i++) {
-    for (int j=0;j<Ny;j++) {
-      m_udothat[i][j] = -m_gamma[i][j]*((m_tafhat[i][j] - m_tafhat0)*m_iceDensity*m_gravity + 
-                                        m_beta[i][j]*m_uhat[i][j])*SECSPERYEAR;
-      m_uhat[i][j] += m_udothat[i][j]*m_dt;
+  // Update transformed velocity and uplift fields using Bueler, et al. 2007, eq 11. 
+  IntVect loVect = IntVect::Zero;
+  IntVect hiVect(Nx-1, Ny-1);
+  Box domBox(loVect, hiVect);
+  Vector<Box> thisVectBox(1);
+  thisVectBox[0] = domBox;
+  Vector<int> procAssign(1,0);
+  DisjointBoxLayout dbl(thisVectBox, procAssign);
+  DataIterator dit = dbl.dataIterator();
+
+  for (dit.begin();dit.ok();++dit) {
+    // grab this FAB
+    FArrayBox& m_betaFAB = m_beta[dit];
+    FArrayBox& m_gammaFAB = m_gamma[dit];
+
+    FArrayBox& m_m_tafhatFAB = m_tafhat[dit];
+    FArrayBox& m_udothatFAB = m_udothat[dit];
+    FArrayBox& m_uhatFAB = m_uhat[dit];
+
+    BoxIterator bit(m_betaFAB.box());
+    for (bit.begin(); bit.ok(); ++bit) {
+      IntVect iv = bit();
+      int comp = 0;
+      m_udothat(iv,comp) = -m_gamma(iv,comp)*((m_tafhat(iv,comp) - m_tafhat0)*m_iceDensity*m_gravity + 
+                                        m_beta(iv,comp)*m_uhat(iv,comp))*SECSPERYEAR;
+      m_uhat(iv,comp) += m_udothat(iv,comp)*m_dt;
     }
   }
 
@@ -312,76 +287,76 @@ BuelerTopgFlux::fftinv () {
 }
 
 // A square load in the center of an Nx x Ny domain, for testing.
-MatDoub squareLoad(int Nx, int Ny) {
-  MatDoub l(Nx, Ny);
-  for (int i=0;i<Nx;i++) {
-    for (int j=0;j<Ny;j++) {
-      float ni = float(i)/float(Nx);
-      float nj = float(j)/float(Ny);
-      if ( ni > 0.333 && ni < 0.666 && nj > 0.333 && nj < 0.666 ) { 
-        l[i][j] = 1.;
-      }
-      else {
-        l[i][j] = 0.;
-      }
-    }
-  }
-  return  l;
-}
-
-// Periodic loads in x and y.
-MatDoub cosineLoad(int Nx, int Ny, float fx, float fy) {
-  MatDoub l(Nx, Ny);
-  for (int i=0;i<Nx;i++) {
-    for (int j=0;j<Ny;j++) {
-      float ni = float(i)/float(Nx);
-      float nj = float(j)/float(Ny);
-      l[i][j] = cos(PI2*fx*ni)*cos(PI2*fy*nj);
-    }
-  }
-  return  l;
-}
-
-int main(int argc, char** argv) {
-  if (argc > 1) {
-    float TMAX = 10;
-    string test = argv[1];
-    const int Nx = stoi(argv[2]);
-    const int Ny = stoi(argv[3]); 
-    float fx, fy;
-    if (test == "periodic" && argc > 3) {
-      fx = stof(argv[4]);
-      fy = stof(argv[5]);
-    }
-      
-    MatDoub l;
-    if (test == "square"){ 
-      l=squareLoad(Nx,Ny); 
-    }
-    else if (test == "periodic"){
-      l=cosineLoad(Nx,Ny,fx,fy);
-    }
-  
-    BuelerTopgFlux flux(Nx, Ny, 128000., 128000., 1., 1);
-    int t = 0;
-    while (t<TMAX) {   
-      flux.updateUdot(l);
-      string fname = test+"test_t"+std::to_string(t)+".txt";
-      savemat(flux.Un, fname);
-      t++;
-    }
-  
-    // learning fftw
-    //MatDoub lhat(Nx*2,Ny*2);
-    //fftw_plan fftfor = fftw_plan_r2r_2d(Nx, Ny, &flux.dL[0][0], &flux.dLhat[0][0], FFTW_DHT, FFTW_DHT, FFTW_ESTIMATE);
-    //fftw_plan fftinv = fftw_plan_r2r_2d(Nx, Ny, &flux.dLhat[0][0], &flux.dL[0][0], FFTW_DHT, FFTW_DHT, FFTW_ESTIMATE);
-    //fftw_execute(fftfor);
-    //fftw_execute(fftfor);
-    //fftw_execute(fftinv);
-    //savemat(flux.k, "test.txt");
-  }
-
-  return 0;
-}
+//MatDoub squareLoad(int Nx, int Ny) {
+//  MatDoub l(Nx, Ny);
+//  for (int i=0;i<Nx;i++) {
+//    for (int j=0;j<Ny;j++) {
+//      float ni = float(i)/float(Nx);
+//      float nj = float(j)/float(Ny);
+//      if ( ni > 0.333 && ni < 0.666 && nj > 0.333 && nj < 0.666 ) { 
+//        l[i][j] = 1.;
+//      }
+//      else {
+//        l[i][j] = 0.;
+//      }
+//    }
+//  }
+//  return  l;
+//}
+//
+//// Periodic loads in x and y.
+//MatDoub cosineLoad(int Nx, int Ny, float fx, float fy) {
+//  MatDoub l(Nx, Ny);
+//  for (int i=0;i<Nx;i++) {
+//    for (int j=0;j<Ny;j++) {
+//      float ni = float(i)/float(Nx);
+//      float nj = float(j)/float(Ny);
+//      l[i][j] = cos(PI2*fx*ni)*cos(PI2*fy*nj);
+//    }
+//  }
+//  return  l;
+//}
+//
+//int main(int argc, char** argv) {
+//  if (argc > 1) {
+//    float TMAX = 10;
+//    string test = argv[1];
+//    const int Nx = stoi(argv[2]);
+//    const int Ny = stoi(argv[3]); 
+//    float fx, fy;
+//    if (test == "periodic" && argc > 3) {
+//      fx = stof(argv[4]);
+//      fy = stof(argv[5]);
+//    }
+//      
+//    MatDoub l;
+//    if (test == "square"){ 
+//      l=squareLoad(Nx,Ny); 
+//    }
+//    else if (test == "periodic"){
+//      l=cosineLoad(Nx,Ny,fx,fy);
+//    }
+//  
+//    BuelerTopgFlux flux(Nx, Ny, 128000., 128000., 1., 1);
+//    int t = 0;
+//    while (t<TMAX) {   
+//      flux.updateUdot(l);
+//      string fname = test+"test_t"+std::to_string(t)+".txt";
+//      savemat(flux.Un, fname);
+//      t++;
+//    }
+//  
+//    // learning fftw
+//    //MatDoub lhat(Nx*2,Ny*2);
+//    //fftw_plan fftfor = fftw_plan_r2r_2d(Nx, Ny, &flux.dL[0][0], &flux.dLhat[0][0], FFTW_DHT, FFTW_DHT, FFTW_ESTIMATE);
+//    //fftw_plan fftinv = fftw_plan_r2r_2d(Nx, Ny, &flux.dLhat[0][0], &flux.dL[0][0], FFTW_DHT, FFTW_DHT, FFTW_ESTIMATE);
+//    //fftw_execute(fftfor);
+//    //fftw_execute(fftfor);
+//    //fftw_execute(fftinv);
+//    //savemat(flux.k, "test.txt");
+//  }
+//
+//  return 0;
+//}
 
 #include "NamespaceFooter.H"
